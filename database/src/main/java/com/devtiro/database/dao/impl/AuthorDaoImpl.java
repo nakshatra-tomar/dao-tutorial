b0b07644987aff4fcb15ba34d00f7873ee6c2bcd
nakshatra-tomar <tomarnakshatra@gmail.com>
2024-08-25 23:20:42 +0530

    added DAO impls and unit tests

--- database/src/main/java/com/devtiro/database/dao/impl/AuthorDaoImpl.java
+++ database/src/main/java/com/devtiro/database/dao/impl/AuthorDaoImpl.java
@@ -1,6 +1,7 @@
 package com.devtiro.database.dao.impl;
 
 import com.devtiro.database.dao.AuthorDao;
+import com.devtiro.database.domain.Author;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class AuthorDaoImpl implements AuthorDao {
@@ -9,5 +10,14 @@ public class AuthorDaoImpl implements AuthorDao {
 
     public AuthorDaoImpl(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    @Override
+    public void create(Author author) {
+        jdbcTemplate.update(
+                "INSERT INTO authors (id, name, age) VALUES (?, ?, ?)",
+                author.getId(), author.getName(), author.getAge()
+        );
     }
 }
